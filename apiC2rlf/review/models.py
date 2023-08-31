6ead4b98216982bfe98e05942fd5df957f479e46
ChrisBonheur <bonheurmafoundou@gmail.com>
2023-08-31 13:53:25 +0100

    Add new model numero

--- apiC2rlf/review/models.py
+++ apiC2rlf/review/models.py
@@ -6,3 +6,9 @@ class Volume(models.Model):
     pages_number = models.PositiveSmallIntegerField()
     volume_year = models.PositiveIntegerField(unique=True)
     
+
+class Numero(models.Model):
+    volume = models.ForeignKey(Volume, on_delete=models.CASCADE)
+    datecreation = models.DateField(auto_now_add=True)
+    label = models.CharField(max_length=100)
+    number = models.PositiveSmallIntegerField()
