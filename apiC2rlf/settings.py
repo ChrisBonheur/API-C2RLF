e7c50539c4a346ff414281bf0c2baa2cd3cb32e0
ChrisBonheur <bonheurmafoundou@gmail.com>
2023-10-01 15:05:58 +0100

    Activate prod db

--- apiC2rlf/settings.py
+++ apiC2rlf/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'apiC2rlf.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 #if os.environ.get('ENV') == 'PRODUCTION':
-"""
+#"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -111,7 +111,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
